7d28be26db50f4d0ca05c2ad7157e4add222ae50
Guru Dheeraj Reddy <kambalagurudheerajreddy@gmail.com>
2026-01-03 15:22:13 +0530

    added footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -56,6 +56,9 @@ function App() {
           deleteContact={deleteContact}
         />
       </div>
+      <footer className="app-footer">
+        &copy; Kambala Guru Dheeraj Reddy
+      </footer>
     </div>
   );
 }
